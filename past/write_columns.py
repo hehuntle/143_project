4b9b01840f70d812c0b8de1c73808c6f8870e043
hehuntle <59852475+hehuntle@users.noreply.github.com>
2021-02-06 19:43:21 -0800

    Delete past directory

--- past/write_columns.py
+++
@@ -1,9 +0,0 @@
-def write_columns(data,fname)
-
-    '''
-    function that takes int input then returns weights for list to sum to 
-    input value
-    
-    :param item: input char
-    :type item: list
-    '''
